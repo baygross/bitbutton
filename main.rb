1c14d0d67cea0d7b0935915e07ba29e5cec5130d
Bay Gross <bgross17@gmail.com>
2017-08-04 23:58:07 -0400

    new styles

--- main.rb
+++ main.rb
@@ -12,22 +12,35 @@ end
 
 get_or_post '/' do
 
-  @transactions = @@account.transactions.reverse
+  @btc_balance = @@client.accounts.select{|a| a.name =="BTC Wallet"}[0]['native_balance']['amount'].to_f.round(0)
+  @usd_balance = @@client.accounts.select{|a| a.name =="USD Wallet"}[0]['balance']['amount'].to_f.round(0)
+  @total_balance = @usd_balance + @btc_balance
+
+  @transactions = @@account.transactions
   @transactions.map! do |t|
     { :type => t['type'],
       :btc => t['amount']['amount'],
       :usd => t['native_amount']['amount'],
       :time => (Time.parse(t['created_at'])+Time.zone_offset('EDT'))
-               .strftime("%m/%d/%Y at %I:%M%p")
     }
   end
+  @transactions.select!{|t| ['buy', 'sell'].include?( t[:type] )}
+
+  @buy_count = @transactions.count{|t| t[:type]=='buy'}
+  @sell_count = @transactions.count{|t| t[:type]=='sell'}
+  @total_fees = (@buy_count + @sell_count) * -1.59
+  @total_earnings = @total_balance - 700.00
+  @total_net = (@total_earnings - @total_fees).round(2)
+
+  @transactions = @transactions[0..13]
+
 
   erb :index
 end
 
 get_or_post '/buy' do
   begin
-    buy_dollar = 1.01/@@client.buy_price()["amount"].to_f
+    buy_dollar = 10.0/@@client.buy_price()["amount"].to_f
     @@account.buy(amount: buy_dollar, currency: "BTC")
   rescue
     return "ERROR"
@@ -37,7 +50,7 @@ end
 
 get_or_post '/sell' do
   begin
-    sell_dollar = 1.99/@@client.sell_price()["amount"].to_f
+    sell_dollar = 11.49/@@client.sell_price()["amount"].to_f
     @@account.sell(amount: sell_dollar, currency: "BTC")
   rescue
     return "ERROR"
